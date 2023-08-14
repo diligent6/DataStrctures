2c22b3f84f6a0c4263d6f6e26a358233fce6e24c
jiaran <1563826569@qq.com>
2023-08-14 15:03:38 +0800

    完成了01动态数组

---
+++ 01-动态数组/src/com/jiaran/Person.java
@@ -0,0 +1,35 @@
+package com.jiaran;
+
+public class Person {
+    private Integer age;
+    private String name;
+
+    public Person(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
